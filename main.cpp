d78c518234c8843ff68bda23b52af891fa1e07c6
Liang Qi <cavendish.qi@gmail.com>
2024-06-21 10:41:40 +0200

    Play - round 2

--- main.cpp
+++ main.cpp
@@ -3,18 +3,25 @@
 #include <QWidget>
 #include <QHBoxLayout>
 
+class MyWidget : public QWidget {
+public:
+    MyWidget() {
+        setAttribute(Qt::WA_NativeWindow);
+        Widget *win = new Widget();
+        QHBoxLayout *layout = new QHBoxLayout(this);
+        layout->addWidget(win);
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     gst_init(&argc, &argv);
 
-    QWidget *m = new QWidget();
-    Widget *w = new Widget(m);
-    QHBoxLayout *box = new QHBoxLayout(m);
-    box->addWidget(w);
-
-    m->resize(640, 480);
-    m->show();
+    //Widget *win = new Widget();
+    MyWidget *win = new MyWidget();
+    win->resize(640, 480);
+    win->show();
 
     return a.exec();
 }
